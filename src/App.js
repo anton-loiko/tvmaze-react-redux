5872d23fa88546439faff46db10930784adca533
Dementeey <metallist9656@gmail.com>
2018-10-10 15:52:42 +0300

    save

--- src/App.js
+++ src/App.js
@@ -1,77 +1,109 @@
+// import lib
 import React, { Component } from 'react';
 // import { connect } from 'react-redux';
 import { Route, Switch, withRouter } from 'react-router-dom';
+//----------------------------------------- END -----------------------------------------
 
-import Details from './pages/Details';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Logout from './pages/Logout';
-import PageNotFound from './pages/PageNotFound';
-import Registration from './pages/Registration';
-import UserProfile from './pages/UserProfile';
-import Header from './components/Header';
-import Content from './components/Content';
-import PrivateRoute from './components/PrivateRoute';
+// import other
+import Details from 'pages/Details';
+import Home from 'pages/Home';
+import Login from 'pages/Login';
+import Logout from 'pages/Logout';
+import PageNotFound from 'pages/PageNotFound';
+import Registration from 'pages/Registration';
+import UserProfile from 'pages/UserProfile';
+import Header from 'components/Header';
+import Content from 'components/Content';
+import PrivateRoutePropComponent from 'components/PrivateRoutes/PrivateRoutePropComponent';
+import PrivateRoutePropRender from 'components/PrivateRoutes/PrivateRoutePropRender';
+//----------------------------------------- END -----------------------------------------
 
 class App extends Component {
+
+  // login user: user ? set login true and push '/' : set login = false and push '/registration'
   login = user => {
     const localUser= JSON.parse(localStorage.getItem('user'));
 
-    if (!localUser) {
-      return;
-    }
-
-    if (localUser.userName === user.userName
-      && localUser.password === user.password) {
-      localStorage.setItem('login', '1');
-      return this.props.history.push('/');
-    }
-
-    if (user.userName === localUser.userName && user.password !== localUser.password) {
-      return
+    if (localUser) {
+      if (localUser.userName === user.userName && localUser.password === user.password) {
+        localStorage.setItem('login', '1');
+        return this.props.history.push('/');
+      }
+      if (localUser.userName === user.userName && localUser.password !== user.password) {
+        return
+      }
     }
 
     localStorage.setItem('login', '0');
     return this.props.history.push('/registration');
   };
+//----------------------------------------- END -----------------------------------------
 
 
+  // logout user set in localStorage login ='0' (false) and push url '/login
   logout = () => {
     localStorage.setItem('login', '0');
     return this.props.history.push('/login');
   };
+//----------------------------------------- END -----------------------------------------
 
+
+  // registration user set in localStorage login ='1' (true) and push url '/' (home)
   register = () => {
     localStorage.setItem('login', '1');
     return this.props.history.push('/');
   };
+//----------------------------------------- END -----------------------------------------
+
+
+  getStatusLogin = () => !!+localStorage.getItem('login') ;
+//----------------------------------------- END -----------------------------------------
 
   render() {
     // TODO --- переход на Details только из таблицы
 
     return (
-      <div>
-        <Content>
+      <Content>
+        <Route exact path='/' component={Header}/>
+        <Route path='/details' component={Header}/>
+        <Route path='/profile' component={Header}/>
 
-          <Route exact path='/' component={Header}/>
-          <Route path='/details' component={Header}/>
-          <Route path='/profile' component={Header}/>
+        <Switch>
+          {/*<Route path='/registration' render={props => <Registration onRegister={this.register}/>}/>*/}
+          {/*<Route path='/login' render={props => <Login onLogin={this.login}/>}/>*/}
 
-          <Switch>
-            <Route path='/registration' render={props => <Registration onRegister={this.register}/>}/>
-            <Route path='/login' render={props => <Login onLogin={this.login}/>}/>
+          <Route path='/logout' render={props => <Logout onLogout={this.logout}/>}/>
 
-            <Route path='/logout' render={props => <Logout onLogout={this.logout}/>}/>
-            {/*<Route exact path='/' component={Home}/>*/}
+          <PrivateRoutePropRender path='/registration'
+                        login={!this.getStatusLogin()}
+                        toregirect={this.getStatusLogin() ? '/' : '/registration'}
+                        render={props => <Registration onRegister={this.register}/>}/>
+          <PrivateRoutePropRender path='/login'
+                        login={!this.getStatusLogin()}
+                        toregirect={!!this.getStatusLogin() ? '/' : '/login'}
+                        render={props => <Login onLogin={this.login}/>}/>
 
-            <PrivateRoute exact path='/' login={Boolean(+localStorage.getItem('login'))} component={Home}/>
-            <PrivateRoute path='/details' login={Boolean(+localStorage.getItem('login'))} component={Details}/>
-            <PrivateRoute path='/profile' login={Boolean(+localStorage.getItem('login'))} component={UserProfile}/>
-            <Route component={PageNotFound}/>
-          </Switch>
+          {/*<PrivateRoutePropComponent exact path='/'*/}
+                        {/*login={this.getStatusLogin()}*/}
+                        {/*toregirect='/login'*/}
+                        {/*component={Home}/>*/}
 
-        </Content>
-      </div>
+          <PrivateRoutePropRender exact path='/'
+                        login={this.getStatusLogin()}
+                        toregirect='/login'
+                        render={props => <Home state={this.props.state}/>}/>
+
+          <PrivateRoutePropComponent path='/details'
+                        login={this.getStatusLogin()}
+                        toregirect='/login'
+                        component={Details}/>
+          <PrivateRoutePropComponent path='/profile'
+                        login={this.getStatusLogin()}
+                        toregirect='/login'
+                        component={UserProfile}/>
+          <Route component={PageNotFound}/>
+        </Switch>
+      </Content>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+/* eslint-disable indent */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
+// import {createStore, applyMiddleware} from 'redux';
+import {createStore} from 'redux';
 // import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware } from 'redux';
 // import { composeWithDevTools } from 'redux-devtools-extension';
 // import thunk from 'redux-thunk';
-
+import mainReducer from 'reducers';
 import './index.scss';
 import App from './App';
 
+let store = createStore(mainReducer);
+
+// store.dispatch()
+
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <App store={store}/>
   </BrowserRouter>,
   document.getElementById('root')
 );
